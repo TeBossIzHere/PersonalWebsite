9ec1bc20557e945cb496046916534c4ae90cfaab
TeBossIzHere <72513020+TeBossIzHere@users.noreply.github.com>
2021-12-20 09:57:46 -0600

    Added Current Location Feature

--- weatherwebsite.js
+++ weatherwebsite.js
@@ -1,3 +1,10 @@
+// Current Location request
+// localStorage.setItem("currentlocationused", "false");
+if (localStorage.getItem("currentlocationused") === null || localStorage.getItem("currentlocationused") === "false") {
+  addCurrentLocation();
+  localStorage.setItem("currentlocationused", "true");
+}
+// _______________________________________________________________________________
 function linkedinButton() {
   window.open("https://www.linkedin.com/in/muzaffer-ozen/", "_blank").focus();
 }
@@ -65,9 +72,10 @@ createSite();
 // _______________________________________________________________________________
 
 function clearCacheFunction() {
-  let boxNames = ["Houston", "San Francisco"];
-  localStorage.setItem("boxcount", "2");
+  let boxNames = [];
+  localStorage.setItem("boxcount", "0");
   localStorage.setItem("boxnames", JSON.stringify(boxNames));
+  localStorage.setItem("currentlocationused", "false");
   document.location.reload(true);
 }
 
@@ -78,6 +86,21 @@ function addCityFunction() {
   localStorage.setItem("boxcount", boxCount.toString());
   localStorage.setItem("boxnames", JSON.stringify(boxNames));
   document.location.reload(true);
+}
+
+function addCurrentLocation() {
+  $.ajax({
+    url: "https://geolocation-db.com/jsonp",
+    jsonpCallback: "callback",
+    dataType: "jsonp",
+    success: function (location) {
+      boxCount = parseInt(localStorage.getItem("boxcount"));
+      boxCount = boxCount + 1;
+      boxNames[boxCount - 1] = location.city;
+      localStorage.setItem("boxcount", boxCount.toString());
+      localStorage.setItem("boxnames", JSON.stringify(boxNames));
+    }
+  });
 }
 
 //Make it so if its night time the linear gradient for background is darker, and lighter for day time.
@@ -126,63 +149,3 @@ async function createSite() {
       });
   }
 }
-
-// _______________________________________________________________________________
-
-var geocoder;
-
-if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(successFunction, errorFunction);
-}
-//Get the latitude and the longitude;
-function successFunction(position) {
-  var lat = position.coords.latitude;
-  var lng = position.coords.longitude;
-  codeLatLng(lat, lng)
-}
-
-function errorFunction() {
-  alert("Geocoder failed");
-}
-
-function initialize() {
-  geocoder = new google.maps.Geocoder();
-
-
-
-}
-
-function codeLatLng(lat, lng) {
-  var latlng = new google.maps.LatLng(lat, lng);
-  geocoder.geocode({ 'latLng': latlng }, function (results, status) {
-    if (status == google.maps.GeocoderStatus.OK) {
-      console.log(results)
-      if (results[1]) {
-        //formatted address
-        alert(results[0].formatted_address)
-        //find country name
-        for (var i = 0; i < results[0].address_components.length; i++) {
-          for (var b = 0; b < results[0].address_components[i].types.length; b++) {
-
-            //there are different types that might hold a city admin_area_lvl_1 usually does in come cases looking for sublocality type will be more appropriate
-            if (results[0].address_components[i].types[b] == "administrative_area_level_1") {
-              //this is the object you are looking for
-              city = results[0].address_components[i];
-              break;
-            }
-          }
-        }
-        //city data
-        alert(city.short_name + " " + city.long_name)
-
-
-      } else {
-        alert("No results found");
-      }
-    } else {
-      alert("Geocoder failed due to: " + status);
-    }
-  });
-}
-
-// _______________________________________________________________________________
